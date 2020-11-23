abc3a7a0546bb819e1a1fad282f8eea46cb708b3
Su Mei Lee <sumeilee@gmail.com>
2020-11-23 21:56:12 +0800

    adding basic support for tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "tailwindcss/tailwind.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container w-full">
+        <h1 className="text-lg font-sans text-center">GiftHub</h1>
+      </div>
     </div>
   );
 }
