767e238179df021e529173b9e18bad92e0d82cba
Su Mei Lee <sumeilee@gmail.com>
2020-11-24 13:06:45 +0800

    fixing tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "tailwindcss/tailwind.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container w-full">
-        <h1 className="text-lg font-sans text-center">GiftHub</h1>
+        <h1 className="text-xl font-serif text-center">GiftHub</h1>
       </div>
     </div>
   );
